40ba947f40d94b53f4b3e5a18408d7f0bd4aa96a
Daniil <syvorov.daniil@gmail.com>
2022-06-16 21:41:34 +0700

    tcp server version 2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.9
-RUN pip install --upgrade pip && pip install sockets
+RUN pip install --upgrade pip && pip install sockets && pip install regex
 COPY . .
 EXPOSE 51413
 CMD ["python3", "tcp_server.py"]
--- tcp_server.py
+++ tcp_server.py
@@ -1,22 +1,46 @@
 import socket
+import re
 
-# Задаем адрес сервера
+def correct_data(string):
+    if (re.fullmatch("\d{4}\ [0-9a-fA-F]{2}\ \d{2}\:\d{2}\:\d{2}\.\d{3}\ \d{2}\r\n", string)):
+        data = re.split('\ |\:|\.|\r\n', string)
+        return (data if correct_time(data[2:4]) else False)
+    else:
+        return False
+
+def correct_time(time):
+    if ((0 <= int(time[0]) <= 23) and
+        (0 <= int(time[1]) <= 60) and
+        (0 <= int(time[1]) <= 60)):
+        return True
+    else:
+        return False
+
 SERVER_ADDRESS = ('', 51413)
-
-# Настраиваем сокет
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(SERVER_ADDRESS)
-server_socket.listen(10)
-print('server is running, please, press ctrl+c to stop')
+server_socket.listen(1)
+print("server is running, please, press ctrl+c to stop")
 
-# Слушаем запросы
 while True:
-    connection, address = server_socket.accept()
-    print("new connection from {address}".format(address=address))
-
-    data = connection.recv(1024)
-    print(str(data))
-
-    connection.send(bytes('Hello from server!\n', encoding='UTF-8'))
-
+    try:
+        connection, address = server_socket.accept()
+        data = str(connection.recv(25).decode("UTF-8"))
+    except KeyboardInterrupt:
+        print(" server stopped")
+        break
+    except Exception:
+        connection.send(b"Error!\n")
+        data = ""
+    data = correct_data(data)
+    if (data):
+        data[5] = int(data[5])//100
+        answer = "спортсмен, нагрудный номер {d[0]} прошёл отсечку {d[1]} в {d[2]}:{d[3]}:{d[4]}.{d[5]}\r\n".format(d = data)
+        with open("data.txt", "a") as file:
+            file.write(answer)
+        if (data[6] == "00"):
+            connection.send(bytes(answer, encoding = "UTF-8"))
+    else:
+        connection.send(b"Error format!\n")
     connection.close()
+server_socket.close()
